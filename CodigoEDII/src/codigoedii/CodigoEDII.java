89b9a3460d9d25aa8bf95773e15192dd10a3069f
Rodrigo <Rodrigo@VAIO>
2015-08-02 23:05:32 -0400

    mensaje de hola mundo

--- CodigoEDII/src/codigoedii/CodigoEDII.java
+++ CodigoEDII/src/codigoedii/CodigoEDII.java
@@ -17,6 +17,8 @@ public class CodigoEDII {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
+                
     }
     
 }
